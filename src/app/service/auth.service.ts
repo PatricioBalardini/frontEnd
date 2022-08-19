3b79da34caeef21f24569940bcd3d7385b70ba9a
Patricio Balardini <patriciobalardini@gmail.com>
2022-08-19 03:11:30 +0200

    instalndo firebase

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -9,7 +9,7 @@ import { NuevoUsuario } from '../model/nuevo-usuario';
   providedIn: 'root'
 })
 export class AuthService {
-  authURL = 'https://backend-pb92.herokuapp.com/auth/';
+  authURL = 'https://backendpbe.herokuapp.com/auth/';
 
   constructor(private httpClient: HttpClient) { }
 
--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -8,7 +8,7 @@ import { persona } from '../model/persona.model';
 })
 
 export class PersonaService {
-  URL = 'https://backend-pb92.herokuapp.com/personas/';
+  URL = 'https://backendpbe.herokuapp.com/personas/';
 
   constructor(private http: HttpClient) { }
 
