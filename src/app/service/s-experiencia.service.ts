a5555da6979506a5a9f8fa70cd3c05f143072d30
Patricio Balardini <patriciobalardini@gmail.com>
2022-08-21 13:12:51 +0200

    cambios de disenios

--- src/app/service/s-experiencia.service.ts
+++ src/app/service/s-experiencia.service.ts
@@ -7,7 +7,7 @@ import { Experiencia } from '../model/experiencias';
   providedIn: 'root'
 })
 export class SExperienciaService {
-  expURL = 'https://backend-pb92.herokuapp.com/explab';
+  expURL = 'https://backendpbe.herokuapp.com/explab';
 
   constructor(private httpClient: HttpClient) { }
 
